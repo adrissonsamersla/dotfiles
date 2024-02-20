5c1e0cebda047085dfbd957ab9e939f902d90b6f
Adrisson Samersla <adrissonsamersla@proton.me>
2024-02-20 08:54:29 -0300

    feat: add installation script

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+warn() {
+  echo "WARN: $@" 1>&2
+}
+
+install_deps () {
+    sudo apt-get update && sudo apt-get install neofetch
+}
+
+copy_configs () {
+    BASE=./config
+
+    for ENTRY in $(find ${BASE} -maxdepth 1 -mindepth 1 -type d,f -printf '%f\n')
+    do
+        TARGET="${PWD}/config/${ENTRY}"
+        DESTIN="${HOME}/.config/${ENTRY}"
+
+        if test "${TARGET}"
+        then
+            warn "${ENTRY} already exists in home config folder, overwriting it!";
+            rm -rf "${DESTIN}"
+        fi
+
+        ln -s "${TARGET}" "${DESTIN}"
+    done
+}
+
+copy_configs
